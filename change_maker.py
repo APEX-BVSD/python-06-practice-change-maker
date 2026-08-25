766759b539608f82e36d9a2c8b6e9d39fb494484
jonathoncihlar <jonathoncihlar@github.com>
2026-08-25 07:41:26 -0600

    Add program starter code.

--- change_maker.py
+++ change_maker.py
@@ -4,8 +4,46 @@ First Last - Month Year
 """
 
 def main() -> None:
-    pass
+    pass # remove me
 
+    # TODO: define cent values for coins/bills
+    CENTS_PER_20_DOLLAR_BILL: int = 2000
+    CENTS_PER_10_DOLLAR_BILL: int = 1000
+    CENTS_PER_5_DOLLAR_BILL: int = 500
+    CENTS_PER_1_DOLLAR_BILL: int = 100
+    CENTS_PER_QUARTER: int = 25
+        
+    # TODO: prompt user for amount due and amount paid 
+    amount_due: float
+    amount_paid: float
+
+    # TODO: check that amount due and amound paid are greater than 0
+
+    # TODO: check that amount paid is greater than amount due
+
+    # TODO: calculate change
+    change: float
+
+    # TODO: convert change to cents - it will make calculating the amount of coins/bills easier
+    change_in_cents: int 
+
+    # TODO: calculate the number of 20 dollar bills needed
+    count_20_dollar_bill: int
+    # TODO: update the change in cents to how many are left over
+
+    # TODO: calculate the number of 10 dollar bills needed
+    count_10_dollar_bill: int
+    # TODO: update the change in cents to how many are left over
+
+    # TODO: calculate the number of 5 dollar bills needed
+    count_5_dollar_bill: int
+    # TODO: update the change in cents to how many are left over
+
+
+    # TODO: output change
+    print(f"Change: ")
+    # TODO: output amount of each bill/coin
+    print(f"Twenty-dollar bills: ")
 
 if __name__ == "__main__":
     main()
